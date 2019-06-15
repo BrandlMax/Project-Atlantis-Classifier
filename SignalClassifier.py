4c6f07cb3386eec1eca01cbae36a8db4ceb1c9c7
brandlmax <kontakt@brandl-maximilian.de>
2019-06-15 11:48:10 +0200

    TimeSheet

---
+++ SignalClassifier.py
@@ -0,0 +1,31 @@
+
+# PART II
+
+# Preparation
+# UI Select
+# -> Train(mode) // mode = 'heatmap' oder 'spectrogram'
+
+# prepareData()
+# Nimmt die CSV Dateien aus dem data-Folder
+# generiert daraus Labels und Images aus den Plots: Label_train_01.jpg / Label_test_01.jpg
+# 80% Testdaten + 20% Validation in extra train_data Folder.
+
+# Classifier
+# trainModel()
+# Nimmt die Daten und trainiert das netzwerk
+# ImageClassifier(labels[]) -> returns Loss
+
+# validateModel()
+# nimmt die Testdaten und predicted
+# returns accuracy
+
+# PART II
+
+# Port, // Serial Port
+# baudRate, // Baudrate from Arduino
+# freqLength=160, // Number of related values for Plotting
+# startValue='999' // start value (kind of start bit)
+# -> Predict(mode, serialPort, baudRate, freqLength, startValue) // mode = 'heatmap' oder 'spectrogram'
+# Get Serial Stream
+# Ploting()
+# ImageClassifier_Predict(ImageAsBase64) -> Label + Probability
