fcd2ab7d73cc0dae2670c6e692f32a493f7e30fe
Philipp Kaltofen <PhilippKaltofen@users.noreply.github.com>
2019-07-02 13:05:31 +0200

    RT Plotting works

--- realtimePlottingZwei.py
+++ realtimePlottingZwei.py
@@ -14,9 +14,7 @@ Divider = "999"
 SERIAL = Serial.CONNECTION(Port, Baud, BufferLength, Divider)
 
 # PLOT
-fig = plt.figure()
-ax = plt.axes()
-line, = ax.plot([], [], lw=2)
+fig, ax = plt.subplots()
 
 def init():
     line.set_data([], [])
@@ -26,22 +24,16 @@ def animate(i):
    if (SERIAL.ready):
         SERIAL.READ()
 
-        # GET DATA
-        y = SERIAL.doneBUFFER.copy()
-        
-        x = range(len(y))
-        
-        # EXTEND
-        extent = [x[0]-(x[1]-x[0])/2., x[-1]+(x[1]-x[0])/2.,0,1]
-        ax.imshow(y[np.newaxis,:], cmap="plasma", aspect="auto", extent=extent)
-        ax.set_yticks([])
-        ax.set_xlim(extent[0], extent[1])
+        y = np.array(SERIAL.doneBUFFER.copy())
+        x = np.linspace(0, 160, 160)     
 
-        # PLOT
-        line.set_data(x, y)
-        ax.plot(x, y, lw=2)
-        return line,
+        extent = [x[0]-(x[1]-x[0])/2., x[-1]+(x[1]-x[0])/2.,0,1]
+        
+        ax.cla()
+        ax.imshow(y[np.newaxis,:], cmap="plasma", aspect="auto", extent=extent)
+        ax.set_title("frame {}".format(i))
+        plt.pause(0.001)
        
-anim = animation.FuncAnimation(fig, animate, init_func = init, frames = 200, interval = 20, blit = True)
+anim = animation.FuncAnimation(fig, animate, interval = 1)
 
 plt.show()
